86ae794aa63a10652a9d242d9be0f16a7217e405
Yuhei Shibata <shibattyo.obama@gmail.com>
2022-08-13 21:18:27 +0900

    finish api-client

--- next.config.js
+++ next.config.js
@@ -16,6 +16,15 @@ const nextConfig = {
 
     return compilerConfig;
   })(),
+  // ReWritesは指定したURLパターンを内部で別のURLに変換する仕組み
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/services/users/get-user.ts
@@ -0,0 +1,24 @@
+import { ApiContext, User } from "types/data";
+import { fetcher } from "utils";
+
+export type GetUserParams = {
+  id: number;
+};
+
+const getUser = async (
+  context: ApiContext,
+  { id }: GetUserParams
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, "")}/users/${id}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
+
+export default getUser;
